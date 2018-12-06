84ab070c089b37d684b53eb27b3e0b91f15e4597
alvarobg94 <alvarobg94@hotmail.com>
2018-12-06 15:28:15 +0100

    re

--- S_csv_ns.py
+++ S_csv_ns.py
@@ -13,45 +13,45 @@ cl_vector_s=[-1]*4
 cl_vector_r=[-1]*8
 #server bind socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-#serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostbyname(socket.gethostname())
 port = 9999                                          
 serversocket.bind((host, port)) 
 
-########### SSH pi in the rotor
-# PARAMETERS 
-SSH_ADDRESS = "thesis"
-SSH_USERNAME = "pi"
-SSH_PASSWORD = "thesis"
-SSH_COMMAND = "python ~/raspberry_rotor/client_rotor_1.py "+socket.gethostbyname(socket.gethostname())
-#SSH creation
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_stdin = ssh_stdout = ssh_stderr = None
-# Order 
-try:
-    ssh.connect(SSH_ADDRESS, username=SSH_USERNAME, password=SSH_PASSWORD)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND)
-except Exception as e:
-    sys.stderr.write("SSH connection error: {0}".format(e))
-    print('ssh error')
-############# SSH pi in the estator
-# PARAMETERS
-SSH_ADDRESS2 = "thesis2"
-SSH_USERNAME2 = "pi"
-SSH_PASSWORD2 = "thesis2"
-SSH_COMMAND2 = "python ~/raspberry_stator/client_stator_1.py "+socket.gethostbyname(socket.gethostname())
-# SSH creation
-ssh2 = paramiko.SSHClient()
-ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_stdin = ssh_stdout = ssh_stderr = None
- #Order
-try:
-    ssh2.connect(SSH_ADDRESS2, username=SSH_USERNAME2, password=SSH_PASSWORD2)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh2.exec_command(SSH_COMMAND2)
-except Exception as e:
-    sys.stderr.write("SSH connection error: {0}".format(e))
-    print('ssh error')
+# ########### SSH pi in the rotor
+# # PARAMETERS 
+# SSH_ADDRESS = "thesis"
+# SSH_USERNAME = "pi"
+# SSH_PASSWORD = "thesis"
+# SSH_COMMAND = "python ~/raspberry_rotor/client_rotor_1_csv.py "+socket.gethostbyname(socket.gethostname())
+# #SSH creation
+# ssh = paramiko.SSHClient()
+# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# ssh_stdin = ssh_stdout = ssh_stderr = None
+# # Order 
+# try:
+#     ssh.connect(SSH_ADDRESS, username=SSH_USERNAME, password=SSH_PASSWORD)
+#     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND)
+# except Exception as e:
+#     sys.stderr.write("SSH connection error: {0}".format(e))
+#     print('ssh error')
+# ############# SSH pi in the estator
+# # PARAMETERS
+# SSH_ADDRESS2 = "thesis2"
+# SSH_USERNAME2 = "pi"
+# SSH_PASSWORD2 = "thesis2"
+# SSH_COMMAND2 = "python ~/raspberry_stator/client_stator_1_csv.py "+socket.gethostbyname(socket.gethostname())
+# # SSH creation
+# ssh2 = paramiko.SSHClient()
+# ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# ssh_stdin = ssh_stdout = ssh_stderr = None
+#  #Order
+# try:
+#     ssh2.connect(SSH_ADDRESS2, username=SSH_USERNAME2, password=SSH_PASSWORD2)
+#     ssh_stdin, ssh_stdout, ssh_stderr = ssh2.exec_command(SSH_COMMAND2)
+# except Exception as e:
+#     sys.stderr.write("SSH connection error: {0}".format(e))
+#     print('ssh error')
 
 
 # queue up to the 2 requests
@@ -63,21 +63,37 @@ def threaded_server():
     i=0
     global data_arr
     while c_flag:
-        
+        data_actuation_s=[0]*4
+        data_actuation_r=[0]*8
         # establish a connection
         r_socket,addr_r = serversocket.accept()
         print("Got a connection from %s the rotor" % str(addr_r))
         s_socket,addr_s = serversocket.accept() 
         print("Got a connection from %s the stator" % str(addr_s))  
-       
+        data_sensor_rj = r_socket.recv(4096)
+        data_sensor_sj = s_socket.recv(4096)
+        load_sensor_r = json.loads(data_sensor_rj.decode())
+        load_sensor_s = json.loads(data_sensor_sj.decode())
+        cal_r=load_sensor_r.get("adc_values_r_cal")
+        cal_s=load_sensor_s.get("adc_values_s_cal")
+        data_send_r = json.dumps({"act_values_r": data_actuation_r})
+        data_send_s = json.dumps({"act_values_s": data_actuation_s})
+        r_socket.send(data_send_r.encode())
+        s_socket.send(data_send_s.encode())
+        print(cal_r)
+        print(cal_s)
         j=0
         while c_flag:
             i+=1
             t1=time.time()
             ### SENSING
+            print("esperando r")
             data_sensor_rj = r_socket.recv(4096)
+            print("esperando s")
             data_sensor_sj = s_socket.recv(4096)
+            print("load r")
             load_sensor_r = json.loads(data_sensor_rj.decode())
+            print("load s")
             load_sensor_s = json.loads(data_sensor_sj.decode())
             data_sensor_r=load_sensor_r.get("adc_values_r")
             data_sensor_s=load_sensor_s.get("adc_values_s")
@@ -117,7 +133,7 @@ for i in range(1):
     t.daemon = True
     threads.append(t)
     t.start()
-
+time.sleep(5)
 while s_flag:
     command=input("Introduce your command")
     if command=="c":
--- Server_mass.py
+++ Server_mass.py
@@ -6,6 +6,8 @@ import sys
 import json
 global c_flag
 import time
+import numpy as np
+import csv
 
 c_flag=1
 s_flag=1
@@ -13,7 +15,7 @@ cl_vector_s=[-1]*4
 cl_vector_r=[-1]*8
 #server bind socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-#serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostbyname(socket.gethostname())
 port = 9999                                          
 serversocket.bind((host, port)) 
@@ -23,7 +25,7 @@ serversocket.bind((host, port))
 SSH_ADDRESS = "thesis"
 SSH_USERNAME = "pi"
 SSH_PASSWORD = "thesis"
-SSH_COMMAND = "python ~/raspberry_rotor/client_rotor_1.py "+socket.gethostbyname(socket.gethostname())
+SSH_COMMAND = "python ~/raspberry_rotor/client_rotor_mass.py "+socket.gethostbyname(socket.gethostname())
 #SSH creation
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -40,7 +42,7 @@ except Exception as e:
 SSH_ADDRESS2 = "thesis2"
 SSH_USERNAME2 = "pi"
 SSH_PASSWORD2 = "thesis2"
-SSH_COMMAND2 = "python ~/raspberry_stator/client_stator_1.py "+socket.gethostbyname(socket.gethostname())
+SSH_COMMAND2 = "python ~/raspberry_stator/client_stator_mass.py "+socket.gethostbyname(socket.gethostname())
 # SSH creation
 ssh2 = paramiko.SSHClient()
 ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -61,18 +63,34 @@ serversocket.listen(2)
                                          
 def threaded_server():
     i=0
-    global data_arr
+    data_actuation_s=[0]*4
+    data_actuation_r=[0]*8
+    states=[0]*7
+    states_nc=[0]*7
+    state_vector=[]
+    time_series=[]
+    tnext=0
+    # establish a connection
+    r_socket,addr_r = serversocket.accept()
+    print("Got a connection from %s the rotor" % str(addr_r))
+    s_socket,addr_s = serversocket.accept() 
+    print("Got a connection from %s the stator" % str(addr_s))  
+    # receive calibrated data
+    data_sensor_rj = r_socket.recv(4096)
+    data_sensor_sj = s_socket.recv(4096)
+    load_sensor_r = json.loads(data_sensor_rj.decode())
+    load_sensor_s = json.loads(data_sensor_sj.decode())
+    cal_r=load_sensor_r.get("adc_values_r_cal")
+    cal_s=load_sensor_s.get("adc_values_s_cal")
+    print(cal_s)
+    print(cal_r)
+    data_send_r = json.dumps({"act_values_r": data_actuation_r})
+    data_send_s = json.dumps({"act_values_s": data_actuation_s})
+    r_socket.send(data_send_r.encode())
+    s_socket.send(data_send_s.encode())
+    j=0
     while c_flag:
-        
-        # establish a connection
-        r_socket,addr_r = serversocket.accept()
-        print("Got a connection from %s the rotor" % str(addr_r))
-        s_socket,addr_s = serversocket.accept() 
-        print("Got a connection from %s the stator" % str(addr_s))  
-       
-        j=0
-        while c_flag:
-            i+=1
+            i=i+1
             t1=time.time()
             ### SENSING
             data_sensor_rj = r_socket.recv(4096)
@@ -81,16 +99,25 @@ def threaded_server():
             load_sensor_s = json.loads(data_sensor_sj.decode())
             data_sensor_r=load_sensor_r.get("adc_values_r")
             data_sensor_s=load_sensor_s.get("adc_values_s")
-            print(data_sensor_r)
-            print(data_sensor_s)
+            for k in range(7):
+                if k <4:
+                    states_nc[k]=data_sensor_r[k]
+                    states[k]=states_nc[k]-cal_r[k]
+                else:
+                    states_nc[k]=data_sensor_s[k-4]
+                    states[k]=states_nc[k]-cal_s[k-4]
+                states[k] = states[k]*4.096/32768
+                states[k] = states[k]*1.5/2
+            state_vector.extend(states)
+            print(states)
+            print(states_nc)
             #### ACTUATING
             ### Control computation
-            data_actuation_s = ([100-j,100-j,100-j,100-j])  #4 external magnets
+            for p in range(4):
+                data_actuation_s[p] = 50*float(np.random.rand(1,1,1))  #4 external magnets
             ##                   |-pair 1-|  |-pair 2-| |--pair 3-|
-            data_actuation_r = ([100-j,100-j,100-j,100-j,100-j,100-j,100-j,100-j]) #8 rotor magnets
-            j+=1
-            if(j>100):
-                j=0
+            for p in range(8):
+                data_actuation_r[p] =50*float(np.random.rand(1,1,1))
             #### end of control computation
             ### data send has to be a value from 0 to 100
             data_send_r = json.dumps({"act_values_r": data_actuation_r})
@@ -98,18 +125,35 @@ def threaded_server():
             r_socket.send(data_send_r.encode())
             s_socket.send(data_send_s.encode())
             t2=time.time()-t1
-            print(t2)       
+            tnext=tnext+t2
+            time_series.append(tnext)   
             
     
-        close_msg_r = json.dumps({"act_values_r": cl_vector_r})
-        r_socket.send(close_msg_r.encode())
-        close_msg_s = json.dumps({"act_values_s": cl_vector_s})
-        s_socket.send(close_msg_s.encode())  
-        time.sleep(0.2)
-        print("cerrando socket")
-        r_socket.close()
-        s_socket.close()
-        print("socket cerrado")
+    close_msg_r = json.dumps({"act_values_r": cl_vector_r})
+    r_socket.send(close_msg_r.encode())
+    close_msg_s = json.dumps({"act_values_s": cl_vector_s})
+    s_socket.send(close_msg_s.encode())  
+    time.sleep(0.2)
+    print("cerrando socket")
+    r_socket.close()
+    s_socket.close()
+    print("socket cerrado")
+    state_data=[0]*7
+    length=len(state_vector)
+    n_samples=int(length/7)
+    state_data= [[0 for x in range(8)] for y in range(n_samples)] 
+    c=0
+    for v in range(n_samples):
+        for w in range(8):
+            if w==0:
+                state_data[v][w]=time_series[v]
+            else:
+                state_data[v][w]=state_vector[c]
+                c=c+1
+
+    with open('data_vector_mass.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(state_data)
 # Put the server in a thread
 threads=[]
 for i in range(1):
@@ -117,7 +161,7 @@ for i in range(1):
     t.daemon = True
     threads.append(t)
     t.start()
-
+time.sleep(5)
 while s_flag:
     command=input("Introduce your command")
     if command=="c":
